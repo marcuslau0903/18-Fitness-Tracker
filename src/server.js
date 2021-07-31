979ee17ba9ba8fb18f7eb86c1cf75180e2133785
MarcusLau <marcuslau0903@gmail.com>
2021-08-01 00:05:09 +0100

    added server, config, db

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const { connect } = require("./db");
+const routes = require("./routes");
+const PORT = process.env.PORT || 4000;
+const app = express();
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "./public")));
+// router
+app.use(routes);
+// connect to database and start server
+const init = async () => {
+  await connect();
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+init();
