cf4bf05ee5b224c0c734f6ac238978573572daa5
surajverma2587 <surajverma2587@gmail.com>
2021-08-06 13:32:44 +0100

    update config

--- src/config.js
+++ src/config.js
@@ -1,14 +1,15 @@
-const PORT = process.env.PORT || 4004;
-const DB_NAME = "workout";
+const DB_NAME = process.env.DB_NAME || "workout";
+
 const DB_URL = process.env.MONGODB_URI || `mongodb://localhost/${DB_NAME}`;
+
 const MONGOOSE_OPTIONS = {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false,
 };
+
 module.exports = {
-  PORT,
   DB_URL,
   MONGOOSE_OPTIONS,
 };
