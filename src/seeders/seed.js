e0521316077a8ad7cd84d08a40acf157c3d1f485
surajverma2587 <surajverma2587@gmail.com>
2021-08-06 13:33:41 +0100

    udpate seed

--- src/seeders/seed.js
+++ src/seeders/seed.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
-
+const { DB_URL, MONGOOSE_OPTIONS } = require("../config");
 const db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(DB_URL, MONGOOSE_OPTIONS);
 
 const workoutSeed = [
   {
